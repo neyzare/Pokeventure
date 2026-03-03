01fe1e65b3ca69baf35e529dc11bde5cb38e22bb
neyzar <lucas.narguet451@gmail.com>
2026-03-03 12:19:10 +0100

    fix: vercel.json

--- api/index.js
+++ api/index.js
@@ -1,9 +1,46 @@
 import { renderPage } from "vike/server";
+import { telefunc } from "telefunc";
 
+/**
+ * Vercel serverless handler for SSR + Telefunc
+ * @param {import('http').IncomingMessage} req
+ * @param {import('http').ServerResponse} res
+ */
 export default async function handler(req, res) {
-    const { httpResponse } = await renderPage({
-        urlOriginal: req.url,
-    });
+    const url = req.url ?? "/";
+
+    // Handle Telefunc RPC
+    if (url.startsWith("/_telefunc")) {
+        const contentType = req.headers["content-type"] || "";
+
+        // Add res.clearCookie for logout (Express compatibility)
+        const resWithCookie = Object.assign(res, {
+            clearCookie(name, opts = {}) {
+                const { path = "/", ...rest } = opts;
+                res.setHeader(
+                    "Set-Cookie",
+                    `${name}=; Path=${path}; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly`
+                );
+            },
+        });
+
+        const httpResponse = await telefunc({
+            url,
+            method: req.method ?? "GET",
+            readable: req,
+            contentType,
+            context: { req, res: resWithCookie },
+        });
+
+        res.statusCode = httpResponse.statusCode;
+        res.setHeader("content-type", httpResponse.contentType);
+        res.end(httpResponse.body);
+        return;
+    }
+
+    // SSR with Vike
+    const pageContext = await renderPage({ urlOriginal: url });
+    const { httpResponse } = pageContext;
 
     if (!httpResponse) {
         res.statusCode = 404;
